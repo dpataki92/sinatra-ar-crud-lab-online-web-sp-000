8021f974a530520fdff3059da4b3c8f56d38b63f
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-05-13 19:29:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,55 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    "Welcome to the ActiveRecord CRUD lab!"
   end
+
+  #CREATE
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    article = Article.create(title: params[:title], content: params[:content])
+
+    redirect to "/articles/#{article.id}"
+  end
+
+  #READ
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+ #UPDATE
+ get 'articles/:id/edit' do
+  @article = Article.find(params[:id])
+  erb :edit
 end
+
+patch 'articles/:id' do
+  
+  @article = Article.find(params[:id])
+  @article.update(title: params[:title], content: params[:content])
+
+  redirect to "articles/#{@article.id}"
+end
+
+  #DELETE
+  delete 'articles/:id' do
+
+    Article.destroy(params[:id])
+
+    redirect to '/articles'
+
+  end
+
+  end
+
